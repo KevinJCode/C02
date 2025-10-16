f3f5a82efe55915898b812db3a618ca22e91e29d
KevinjCode <megbart@msn.com>
2025-10-16 11:15:34 +0200

    Séparation en 2 fonctions

--- ex02/ipssi_atoi.c
+++ ex02/ipssi_atoi.c
@@ -1,5 +1,25 @@
 #include <unistd.h>
 
+int	ipssi_signe(char *str)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	k = 1;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			j++;
+		i++;
+	}
+	if (j % 2 != 0)
+		k = -1;
+	return (k);
+}
+
 int	ipssi_atoi(char *str)
 {
 	int	i;
@@ -9,18 +29,12 @@ int	ipssi_atoi(char *str)
 
 	i = 0;
 	j = 0;
-	k = 1;
 	l = 0;
 	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\f')
 		i++;
+	k = ipssi_signe(str + i);
 	while (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			j++;
 		i++;
-	}
-	if (j % 2 != 0)
-		k = -1;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		l = l * 10 + (str[i] - '0');
